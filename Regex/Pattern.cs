73cae2e6836668a015ebd920203be277ed47719f
vibha <vibhauc@gmail.com>
2022-03-02 23:35:23 +0530

    [Vibha]Added:User need to enter a valid last name

--- Regex/Pattern.cs
+++ Regex/Pattern.cs
@@ -6,12 +6,26 @@ namespace RegexPattern
     public class Pattern
     {
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$"; //Vba
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$"; //Rao
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9.]+@[A-Za-z0-9]+.[A-Za-z]{2,4}$"; //abc@bl.in
 
         //Method for first name 
         public bool validateFirstName(string fname)
         {
             return Regex.IsMatch(fname, REGEX_FIRSTNAME);
         }
+
         
+        //Method for last name 
+        public bool validateLastName(string lname)
+        {
+            return Regex.IsMatch(lname, REGEX_LASTNAME);
+        }        
+
+        //Method for email 
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
     }
 }
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -7,7 +7,13 @@ namespace RegexPattern
         static void Main(string[] args)
         {
             Pattern pattern = new Pattern();
-            Console.WriteLine(pattern.validateFirstName("Vba"));
+
+            /* Console.WriteLine(pattern.validateFirstName("Vba"));
+             Console.WriteLine(pattern.validateLastName("Rao"));*/
+
+           // string a = Console.ReadLine();
+
+            Console.WriteLine(pattern.validateEmail("abc@bl.in"));
         }
     }
 }
