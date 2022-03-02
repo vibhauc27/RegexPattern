7cfa3b98a8ae62ddef0c197c44c209867e4ef5c2
vibha <vibhauc@gmail.com>
2022-03-02 23:11:01 +0530

    [Vibha]Added:User need to enter a valid first name

---
+++ Regex/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexPattern
+{
+    public class Pattern
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$"; //Vba
+
+        //Method for first name 
+        public bool validateFirstName(string fname)
+        {
+            return Regex.IsMatch(fname, REGEX_FIRSTNAME);
+        }
+        
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.validateFirstName("Vba"));
+        }
+    }
+}
